2218080d5a5abe8a0e09624187e5ce6cd3587b47
Helena Skjaldgaard Rasmussen <hele2536@edu.easj.dk>
2020-12-01 11:06:49 +0100

    version 3.2

--- Norriq-Solutions/Scripts/NORRIQSolutions.js
+++ Norriq-Solutions/Scripts/NORRIQSolutions.js
@@ -95,6 +95,54 @@ function WhichSolutionIsCheckedApplication() {
     return true;
 }
 
+
+function WhichApplicationTypeIsChecked() {
+    var solution1 = document.getElementById("ApplicationSolution1");
+    var solution2 = document.getElementById("ApplicationSolution2");
+    var solution3 = document.getElementById("ApplicationSolution3");
+
+    if (solution1.checked) {
+        window.location.href = "https://localhost:44311/Pages/FunctionPage.html";
+        return false;
+        
+    }
+    if (solution2.checked) {
+        window.location.href = "https://localhost:44311/Pages/FunctionPage.html";
+        return false;
+    }
+    if (solution3.checked) {
+        window.location.href = "https://localhost:44311/Pages/FunctionPage.html";
+        return false;
+    }
+    return true;
+    
+}
+
+function WhichFunctionIsChecked() {
+    var solution1 = document.getElementById("FunctionSolution1");
+    var solution2 = document.getElementById("FunctionSolution2");
+    var solution3 = document.getElementById("FunctionSolution3");
+    var solution4 = document.getElementById("FunctionSolution4");
+
+    if (solution1.checked) {
+        window.location.href = "https://localhost:44311/Pages/LanguagePage.html";
+        return false;
+    }
+    if (solution2.checked) {
+        window.location.href = "https://localhost:44311/Pages/LanguagePage.html";
+        return false;
+    }
+    if (solution3.checked) {
+        window.location.href = "https://localhost:44311/Pages/LanguagePage.html";
+        return false;
+    }
+    if (solution4.checked) {
+        window.location.href = "https://localhost:44311/Pages/LanguagePage.html";
+        return false;
+    }
+    return true;
+}
+
 function selectOnlyThis(id) {
     for (var i = 1; i <= 3; i++) {
         document.getElementById("Solution" + i).checked = false;
@@ -119,6 +167,13 @@ function selectOnlyThisWebshop(id) {
 function selectOnlyThisApplication(id) {
     for (var i = 1; i <= 3; i++) {
         document.getElementById("ApplicationSolution" + i).checked = false;
+    }
+    document.getElementById(id).checked = true;
+}
+
+function selectOnlyThisFunction(id) {
+    for (var i = 1; i <= 3; i++) {
+        document.getElementById("FunctionSolution" + i).checked = false;
     }
     document.getElementById(id).checked = true;
 }
